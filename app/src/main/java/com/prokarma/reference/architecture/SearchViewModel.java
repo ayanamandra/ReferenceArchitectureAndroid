d2bb1fc5ef3ddc5440a3fe15ba1aaba5ae862704
Erick Garcia <ergarcia@prokarma.com>
2018-08-08 16:29:07 -0700

    Modified the search activity design to better match the given comp. Organized the code into regions.

--- app/src/main/java/com/prokarma/reference/architecture/SearchViewModel.java
+++ app/src/main/java/com/prokarma/reference/architecture/SearchViewModel.java
@@ -10,19 +10,28 @@ public class SearchViewModel extends ViewModel {
 
     private final String TAG = "SearchViewModel";
 
+    //region Instance variables
     private String mSearchQuery;
+    private String mSearchKeyword;
+    //endregion
 
+    //region Constructors
     public SearchViewModel() {
         mSearchQuery = "";
+        mSearchKeyword = "";
     }
+    //endregion
 
+    //region Public methods
     /**
      * Search for related events to the given input.
      */
     public void search() {
-        Log.d(TAG, "Search event triggered: " + mSearchQuery);
+        Log.d(TAG, "Search event triggered: query = " + mSearchQuery + " keyword: " + mSearchKeyword);
     }
+    //endregion
 
+    //region Accessors and Mutators
     public String getSearchQuery() {
         return mSearchQuery;
     }
@@ -30,4 +39,13 @@ public class SearchViewModel extends ViewModel {
     public void setSearchQuery(String searchQuery) {
         mSearchQuery = searchQuery;
     }
+
+    public String getSearchKeyword() {
+        return mSearchKeyword;
+    }
+
+    public void setSearchKeyword(String searchKeyword) {
+        mSearchKeyword = searchKeyword;
+    }
+    //endregion
 }
