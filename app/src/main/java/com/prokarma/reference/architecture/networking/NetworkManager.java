2e2500d3b9cbbe6c86d7706163113138682ff67c
kwelsh <kwelsh@prokarma.com>
2018-08-10 14:47:33 -0400

    Wrote the NetworkManager

--- app/src/main/java/com/prokarma/reference/architecture/networking/NetworkManager.java
+++ app/src/main/java/com/prokarma/reference/architecture/networking/NetworkManager.java
@@ -1,6 +1,57 @@
 package com.prokarma.reference.architecture.networking;
 
+import com.prokarma.reference.architecture.model.SearchEventsResponse;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NetworkManager {
+    private RESTService service = null;
+    private static NetworkManager instance = null;
+    private static OkHttpClient.Builder httpClient = null;
+    public OkHttpClient client;
 
+    private static final String API_KEY = "Ctl1pftvJYMyVJVycySAlLNDVhRMGBMb";
+    private static final String TICKETMASTER_BASE_URL = "https://app.ticketmaster.com/";
+    private static final String TICKETMASTER_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
 
+    public static NetworkManager getInstance() {
+        if (null == instance) {
+            instance = new NetworkManager();
+        }
+        return instance;
+    }
+
+    public void initService() {
+        if (client == null) {
+            initHttpClient();
+            client = httpClient.build();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(TICKETMASTER_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+
+            service = retrofit.create(RESTService.class);
+        }
+    }
+
+    private void initHttpClient() {
+        httpClient = new OkHttpClient.Builder();
+        httpClient.readTimeout(20, TimeUnit.SECONDS);
+        httpClient.connectTimeout(20, TimeUnit.SECONDS);
+    }
+
+    public boolean canMakeNetworkCalls() {
+        return service != null;
+    }
+
+    public Call<SearchEventsResponse> getEvents() {
+        return service.getEvents(TICKETMASTER_URL, API_KEY);
+    }
 }
--- app/src/main/java/com/prokarma/reference/architecture/networking/RESTService.java
+++ app/src/main/java/com/prokarma/reference/architecture/networking/RESTService.java
@@ -5,11 +5,12 @@ import com.prokarma.reference.architecture.model.SearchEventsResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Query;
 import retrofit2.http.Url;
 
 public interface RESTService {
     String API_KEY = "apikey";
 
     @GET
-    Call<SearchEventsResponse> getEvents(@Url String url, @Header(API_KEY) String bearer);
+    Call<SearchEventsResponse> getEvents(@Url String url, @Query(API_KEY) String apikey);
 }
