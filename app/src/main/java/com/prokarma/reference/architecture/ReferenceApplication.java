d936d4c9984b8509cb6a2f2c3dfb819e90f1bbfb
kwelsh <kwelsh@prokarma.com>
2018-08-15 01:07:30 -0400

    Added in an Application class so we can set up stuff like the Networking Service

---
+++ app/src/main/java/com/prokarma/reference/architecture/ReferenceApplication.java
@@ -0,0 +1,16 @@
+package com.prokarma.reference.architecture;
+
+import android.app.Application;
+
+import com.prokarma.reference.architecture.networking.NetworkManager;
+
+public class ReferenceApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //init retrofit service
+        NetworkManager.getInstance().initService();
+    }
+}
