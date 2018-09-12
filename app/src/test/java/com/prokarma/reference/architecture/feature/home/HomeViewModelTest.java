541c59c9c82068681da5b85474b795110acb2eb4
kwelsh <kwelsh@prokarma.com>
2018-09-12 01:36:55 -0400

    Added more to unit test

--- app/src/test/java/com/prokarma/reference/architecture/feature/home/HomeViewModelTest.java
+++ app/src/test/java/com/prokarma/reference/architecture/feature/home/HomeViewModelTest.java
@@ -1,24 +1,32 @@
 package com.prokarma.reference.architecture.feature.home;
 
 import android.content.Context;
+import android.widget.Button;
+
 import com.prokarma.reference.architecture.app.MainActivity;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Mockito.mock;
 
 public class HomeViewModelTest {
 
     private Context context;
     private MainActivity activity;
-    HomeViewModel homeViewModel;
+    private NavigationInterface navigationInterface;
+    private Button button;
+    private HomeViewModel homeViewModel;
 
     @Before
     public void setUp() throws Exception {
         context = mock(Context.class);
         activity = mock(MainActivity.class);
-
-//        homeViewModel = new HomeViewModel();
+        navigationInterface = mock(NavigationInterface.class);
+        button = mock(Button.class);
+        homeViewModel = new HomeViewModel(activity, navigationInterface);
     }
 
     @After
@@ -27,27 +35,39 @@ public class HomeViewModelTest {
 
     @Test
     public void search() {
-        //TODO view required
-        homeViewModel.search(view);
+        homeViewModel.search(button);
     }
 
     @Test
     public void getSearchQuery() {
+        String dummySearchQuery = "Dummy Search Query";
+        homeViewModel.setSearchQuery(dummySearchQuery);
+        Assert.assertEquals(dummySearchQuery, homeViewModel.getSearchQuery());
     }
 
     @Test
     public void setSearchQuery() {
+        String dummySearchQuery = "Dummy Search Query";
+        homeViewModel.setSearchQuery(dummySearchQuery);
+        Assert.assertEquals(dummySearchQuery, homeViewModel.getSearchQuery());
     }
 
     @Test
     public void getSearchKeyword() {
+        String dummySearchKeyword = "Dummy Search Keyword";
+        homeViewModel.setSearchKeyword(dummySearchKeyword);
+        Assert.assertEquals(dummySearchKeyword, homeViewModel.getSearchKeyword());
     }
 
     @Test
     public void setSearchKeyword() {
+        String dummySearchKeyword = "Dummy Search Keyword";
+        homeViewModel.setSearchKeyword(dummySearchKeyword);
+        Assert.assertEquals(dummySearchKeyword, homeViewModel.getSearchKeyword());
     }
 
     @Test
     public void onCleared() {
+
     }
 }
