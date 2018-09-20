aae26835c72bd0e580d477ea6e20157e60c3dccd
unknown <pbhavani@PBHAVANI.pkrmsezdc.com>
2018-09-20 16:03:27 +0530

    covered event search api unit test cases

--- app/src/test/java/com/prokarma/reference/architecture/networking/SearchEventsTest.java
+++ app/src/test/java/com/prokarma/reference/architecture/networking/SearchEventsTest.java
@@ -1,46 +1,67 @@
 package com.prokarma.reference.architecture.networking;
 
+import com.prokarma.reference.architecture.di.AppComponent;
+import com.prokarma.reference.architecture.di.TestModule;
+import com.prokarma.reference.architecture.feature.search.list.DaggerListViewModelTest_TestComponent;
+import com.prokarma.reference.architecture.feature.search.list.ListViewModelTest;
 import com.prokarma.reference.architecture.model.SearchEventsResponse;
 import com.prokarma.reference.architecture.utils.TestUtil;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import dagger.Component;
 import io.reactivex.Single;
 
 public class SearchEventsTest {
 
-
+    @Inject
+    ApplicationDataRepository applicationDataRepository;
     //@Rule
     //public Timeout globalTimeout = Timeout.seconds(20);
 
+    @Before
+    public void setUp() {
+        SearchEventsTest.TestComponent testComponent = DaggerSearchEventsTest_TestComponent.builder().testModule(new TestModule())
+                .build();
+        testComponent.inject(this);
+    }
     @Test
     public void getEvents() {
 //        TestUtil.setupEnvironment();
-        //searchEvents();
-//        searchEvents("NHL");
+        searchEvents();
+        searchEvents("NHL");
     }
 
-//    private SearchEventsResponse searchEvents() {
-//        Single<SearchEventsResponse> call = TicketMasterManager.getInstance().getEvents();
-//
-//        SearchEventsResponse response = call.blockingGet();
-//        Assert.assertNotNull(response);
-//        Assert.assertNotNull(response.getPage());
-//        Assert.assertNotNull(response.getEmbedded());
-//        Assert.assertNotNull(response.getLinks());
-//        return response;
-//    }
+    private SearchEventsResponse searchEvents() {
+        Single<SearchEventsResponse> call = applicationDataRepository.ticketMasterManager.getEvents();
 
-//    private SearchEventsResponse searchEvents(String keyword) {
-//        Single<SearchEventsResponse> call = TicketMasterManager.getInstance().getEvents(keyword);
-//
-//        SearchEventsResponse response = call.blockingGet();
-//        Assert.assertNotNull(response);
-//        Assert.assertNotNull(response.getPage());
-//        Assert.assertNotNull(response.getEmbedded());
-//        Assert.assertNotNull(response.getLinks());
-//        return response;
-//    }
+        SearchEventsResponse response = call.blockingGet();
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getPage());
+        Assert.assertNotNull(response.getEmbedded());
+        Assert.assertNotNull(response.getLinks());
+        return response;
+    }
+
+        private SearchEventsResponse searchEvents(String keyword) {
+        Single<SearchEventsResponse> call = applicationDataRepository.ticketMasterManager.getEvents(keyword);
+
+        SearchEventsResponse response = call.blockingGet();
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getPage());
+        Assert.assertNotNull(response.getEmbedded());
+        Assert.assertNotNull(response.getLinks());
+        return response;
+    }
+    @Singleton
+    @Component(modules = {TestModule.class})
+    interface TestComponent extends AppComponent {
+        void inject(SearchEventsTest test);
+    }
 
 }
